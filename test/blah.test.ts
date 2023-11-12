872062d8a0e8248f87b4d920b68cf7a7cb847d5a
Yelro <50973739+Yelro@users.noreply.github.com>
2023-11-12 08:43:05 -0500

    No ticket: Agregar resta (#1)

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,13 @@
-import { sum } from '../src';
+import { sum, subtract } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('blah', () => {
+  it('works', () => {
+    expect(subtract(1, 1)).toEqual(2);
   });
 });
